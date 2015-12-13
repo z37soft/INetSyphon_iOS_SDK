d26a74d7a9ba02caa6c567a6ffec81b576c6a654
Nozomu MIURA <miura@digitalstage.jp>
2015-12-13 18:16:31 +0800

    Initial Commit

---
+++ INetSyphon_iOS_SDK/INetSyphon_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  INetSyphon_iOS_SDK.h
+//  INetSyphon_iOS_SDK
+//
+//  Created by Nozomu MIURA on 2015/12/13.
+//  Copyright © 2015年 TECHLIFE SG Pte.Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for INetSyphon_iOS_SDK.
+FOUNDATION_EXPORT double INetSyphon_iOS_SDKVersionNumber;
+
+//! Project version string for INetSyphon_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char INetSyphon_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INetSyphon_iOS_SDK/PublicHeader.h>
+
+
